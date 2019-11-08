17084bacc82d9b92ad26f6000a223527d1fc211e
AMC <53350942+AMC-ai@users.noreply.github.com>
2019-11-08 13:30:31 -0500

    i am unable to move fwd with my setup, im goin to restart

--- smurfs/src/components/actions/dataActions.js
+++ smurfs/src/components/actions/dataActions.js
@@ -1,37 +1,37 @@
-import axios from 'axios';
-//fetch
-export const FETCH_POST_LOADING = 'FETCH_POST_LOADING';
-export const FETCH_POST_SUCCESS = 'FETCH_POST_SUCCESS';
-export const FETCH_POST_FAILED = 'FETCH_POST_FAILED ';
-//add
-//do i need to do an add post?
-export const ADD_POST_LOADING = 'ADD_POST_LOADING';
-export const ADD_POST_SUCCESS = 'ADD_POST_SUCCESS';
-export const ADD_POST_FAILED = 'ADD_POST_FAILED ';
-//fetch
-export const getPost = () => {
-    return dispatch => {
-        dispatch({ type: FETCH_POST_LOADING });
-        axios
-            .get('put end point here')
-            .then(res => {
-                dispatch({ type: FETCH_POST_SUCCESS, payload: res.data });
-            })
-            .catch(err => dispatch({ type: FETCH_POST_FAILED, payload: err.res }));
-    };
-};
-//add
-export const addPost = (post) => {
-    return dispatch => {
-        dispatch({ type: ADD_POST_LOADING });
-        axios
-            .post(
-                'put end point here',
-                post
-            )
-            .then(res => {
-                dispatch({ type: ADD_POST_SUCCESS, payload: res.data });
-            })
-            .catch(err => dispatch({ type: ADD_POST_FAILED, payload: err.res }));
-    };
-};
+// import axios from 'axios';
+// //fetch
+// export const FETCH_POST_LOADING = 'FETCH_POST_LOADING';
+// export const FETCH_POST_SUCCESS = 'FETCH_POST_SUCCESS';
+// export const FETCH_POST_FAILED = 'FETCH_POST_FAILED ';
+// //add
+// //do i need to do an add post?
+// export const ADD_POST_LOADING = 'ADD_POST_LOADING';
+// export const ADD_POST_SUCCESS = 'ADD_POST_SUCCESS';
+// export const ADD_POST_FAILED = 'ADD_POST_FAILED ';
+// //fetch
+// export const getPost = () => {
+//     return dispatch => {
+//         dispatch({ type: FETCH_POST_LOADING });
+//         axios
+//             .get('put end point here')
+//             .then(res => {
+//                 dispatch({ type: FETCH_POST_SUCCESS, payload: res.data });
+//             })
+//             .catch(err => dispatch({ type: FETCH_POST_FAILED, payload: err.res }));
+//     };
+// };
+// //add
+// export const addPost = (post) => {
+//     return dispatch => {
+//         dispatch({ type: ADD_POST_LOADING });
+//         axios
+//             .post(
+//                 'put end point here',
+//                 post
+//             )
+//             .then(res => {
+//                 dispatch({ type: ADD_POST_SUCCESS, payload: res.data });
+//             })
+//             .catch(err => dispatch({ type: ADD_POST_FAILED, payload: err.res }));
+//     };
+// };
--- smurfs/src/components/componentFiles/PostForm.js
+++ smurfs/src/components/componentFiles/PostForm.js
@@ -1,78 +1,78 @@
-import React, { useReducer, useState } from 'react';
-import { formReducer, initialState } from '../reducers/formReducer';
+// import React, { useReducer, useState } from 'react';
+// import { formReducer, initialState } from '../reducers/formReducer';
 
-//import action creators
+// //import action creators
 
-//import { connect } from "react-redux";
-
-
-const PostForm = props => {
-    const [state, dispatch] = useReducer(formReducer, initialState);
-
-    const [newPost, setNewPost] = useState({
-        name: '',
-        age: '',
-        height: '',
-    });
-
-    const handleChange = e => {
-        setNewPost({
-            ...newPost,
-            [e.taget.name]:
-                e.target.value
-        });
-    }
-
-    //where is ADD POST coming from? do i need to add a post success?
-
-    const handleSubmit = e => {
-        e.preventDeafault();
-        dispatch({ type: 'ADD_POST', payload: newPost });
-        setNewPost({
-            name: '',
-            age: '',
-            height: '',
-        });
-    }
-
-    retrun(
-        <div className='smurf-form'>
-            <h2>Create Smurf</h2>
-            <form onSubmit={handleSubmit}>
-                <div className='smurf-name'>
-                    <input
-                        type="text"
-                        placeholder='Name'
-                        name='name'
-                        value={newPost.name}
-                        onChange={(e) => handleChange(e)} />
-                </div>
-                <div className='smurf-age'>
-                    <input
-                        type="text"
-                        placeholder='age'
-                        name='age'
-                        value={newPost.age}
-                        onChange={(e) => handleChange(e)} />
-                </div>
-                <div className='smurf-name'>
-                    <input
-                        type="text"
-                        placeholder='height in cm'
-                        name='height'
-                        value={newPost.height}
-                        onChange={(e) => handleChange(e)} />
-                </div>
-                <div>
-                    <button onClick={() => dispatch({ type: 'CREATE_POST' })}>Submit</button>
-                </div>
-            </form>
-        </div>
-    )
+// //import { connect } from "react-redux";
 
 
-}
-export default PostForm;
+// const PostForm = props => {
+//     const [state, dispatch] = useReducer(formReducer, initialState);
+
+//     const [newPost, setNewPost] = useState({
+//         name: '',
+//         age: '',
+//         height: '',
+//     });
+
+//     const handleChange = e => {
+//         setNewPost({
+//             ...newPost,
+//             [e.taget.name]:
+//                 e.target.value
+//         });
+//     }
+
+//     //where is ADD POST coming from? do i need to add a post success?
+
+//     const handleSubmit = e => {
+//         e.preventDeafault();
+//         dispatch({ type: 'ADD_POST', payload: newPost });
+//         setNewPost({
+//             name: '',
+//             age: '',
+//             height: '',
+//         });
+//     }
+
+//     retrun (
+//         <div className='smurf-form'>
+//             <h2>Create Smurf</h2>
+//             <form onSubmit={handleSubmit}>
+//                 <div className='smurf-name'>
+//                     <input
+//                         type="text"
+//                         placeholder='Name'
+//                         name='name'
+//                         value={newPost.name}
+//                         onChange={(e) => handleChange(e)} />
+//                 </div>
+//                 <div className='smurf-age'>
+//                     <input
+//                         type="text"
+//                         placeholder='age'
+//                         name='age'
+//                         value={newPost.age}
+//                         onChange={(e) => handleChange(e)} />
+//                 </div>
+//                 <div className='smurf-name'>
+//                     <input
+//                         type="text"
+//                         placeholder='height in cm'
+//                         name='height'
+//                         value={newPost.height}
+//                         onChange={(e) => handleChange(e)} />
+//                 </div>
+//                 <div>
+//                     <button onClick={() => dispatch({ type: 'CREATE_POST' })}>Submit</button>
+//                 </div>
+//             </form>
+//         </div>
+//     )
+
+
+// }
+// export default PostForm;
 
 // export default connect(
 //     state => state,
